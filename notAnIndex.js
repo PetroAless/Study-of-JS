77bf4c61ddd460842a54ef278f05ad0e8939e62d
Alessio <alessio.petrotta1@gmail.com>
2023-02-03 00:54:06 +0100

    constants function

--- notAnIndex.js
+++ notAnIndex.js
@@ -1,6 +1,13 @@
-var x = 15;
-alert(x);
 function greetMe(stringa){
     alert('hello '+stringa);
 }
-greetMe("world");
+function constants(){
+    const pi=3.14;
+    //defined constants in a function work only in the function's scope
+    alert(pi+'inside function');
+}
+//constants();
+//alert(pi);
+
+
+
