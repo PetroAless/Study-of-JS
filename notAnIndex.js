1314756ba97a77c471810f4ae4537ea7cba9551a
Alessio <alessio.petrotta1@gmail.com>
2023-02-06 16:36:35 +0100

    fix now it works

--- notAnIndex.js
+++ notAnIndex.js
@@ -5,16 +5,18 @@ function constants(){
     const pi=3.14;
     //defined constants in a function work only in the function's scope
     alert(pi+'inside function');
-    //constants cannot be redeclared but the constants properties can be changed
+    //constants properties can be changed
+    const MY_OBJECT = { key: "value" };
+    var g = MY_OBJECT.key;
+    MY_OBJECT.key = "otherValue";
+    g= g + MY_OBJECT.key;
+    alert(g);
     //constants cannot have the same name as a var
+    //constants cannot be redeclared  
 }
 //constants();
 //alert(pi);
-const MY_OBJECT = { key: "value" };
-var g = MY_OBJECT.key;
-MY_OBJECT.key = "otherValue";
-g= g + MY_OBJECT.key;
-alert(g);
+
 
 
 
