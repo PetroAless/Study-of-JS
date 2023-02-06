2e36d497eebadbec08c51fd585cfad02fe089585
Alessio <alessio.petrotta1@gmail.com>
2023-02-06 18:20:18 +0100

    prima assaggio di literal

--- notAnIndex.js
+++ notAnIndex.js
@@ -5,19 +5,24 @@ function constants(){
     const pi=3.14;
     
     alert(pi+'inside function');
-    const MY_OBJECT = { key: "value" };//ex 1
+    const MY_OBJECT = { key: "value" };//Constants ex 1
     var g = MY_OBJECT.key;
     MY_OBJECT.key = "otherValue";
     g = g + MY_OBJECT.key;
     alert(g);
-    //ex 2
-    //ex 3
+    //Constants ex 2
+    //Constants ex 3
+}
+function toString(numero,base){
+    alert(parseInt(numero,base));
+    //toString ex 1
 }
 //greetMe("world");
 //constants();//alert(pi);
-//additions with a number and a string always create a new string
-//eg: "the answer is" + 32 = "the answer is 32"
-//but with all other operators the result is a number
+/*toString("100",2);
+toString("100",10);
+toString("10",16);*/
+
 
 
 
