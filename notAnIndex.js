0cee1bbf34d360cc2e6823612c7fefa005d6ee79
Alessio <alessio.petrotta1@gmail.com>
2023-02-06 16:49:28 +0100

    fixes: order of notes

--- notAnIndex.js
+++ notAnIndex.js
@@ -3,19 +3,21 @@ function greetMe(stringa){
 }
 function constants(){
     const pi=3.14;
-    //defined constants in a function work only in the function's scope
+    
     alert(pi+'inside function');
-    //constants properties can be changed
-    const MY_OBJECT = { key: "value" };
+    const MY_OBJECT = { key: "value" };//ex 1
     var g = MY_OBJECT.key;
     MY_OBJECT.key = "otherValue";
-    g= g + MY_OBJECT.key;
+    g = g + MY_OBJECT.key;
     alert(g);
-    //constants cannot have the same name as a var
-    //constants cannot be redeclared  
+    //ex 2
+    //ex 3
 }
-//constants();
-//alert(pi);
+//greetMe("world");
+//constants();//alert(pi);
+//additions with a number and a string always create a new string
+//eg: "the answer is" + 32 = "the answer is 32"
+//but with all other operators the result is a number
 
 
 
