496d80bed18abfce81195a74f9bb49953b7f17f3
Alessio <alessio.petrotta1@gmail.com>
2023-02-06 18:39:13 +0100

    fix readability

--- notAnIndex.js
+++ notAnIndex.js
@@ -1,27 +1,37 @@
 function greetMe(stringa){
-    alert('hello '+stringa);
+    console.log('hello '+stringa);
 }
 function constants(){
     const pi=3.14;
     
-    alert(pi+'inside function');
-    const MY_OBJECT = { key: "value" };//Constants ex 1
+    console.log(pi+'inside function');
+    const MY_OBJECT = { key: "value" };//Constants ex 1.1
     var g = MY_OBJECT.key;
     MY_OBJECT.key = "otherValue";
     g = g + MY_OBJECT.key;
-    alert(g);
-    //Constants ex 2
-    //Constants ex 3
+    console.log(g);
+    //Constants ex 1.2
+    //Constants ex 1.3
 }
 function toString(numero,base){
-    alert(parseInt(numero,base));
-    //toString ex 1
+    console.log(parseInt(numero,base));
+    //toString ex 3.1
+}
+function literals(){
+    const myLiteralArray=[
+        "first_Property",
+        "second_Property",
+        ,           //ex of TRAILING COMMAS 0.1
+        "third_Property",
+    ];
+    console.log(myLiteralArray[1]);
 }
 //greetMe("world");
-//constants();//alert(pi);
+//constants();//console.log(pi);
 /*toString("100",2);
 toString("100",10);
 toString("10",16);*/
+literals();
 
 
 
